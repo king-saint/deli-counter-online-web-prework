7a3d6bc0af6485766a0ed83275702d19fd17ec81
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 17:54:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(array)
         place += 1
         total_list << "#{place}. #{name}"
       end
-    puts "The line is currently: #{total_list}"
+    puts "The line is currently: #{total_list.join()}"
     end
 end
 
